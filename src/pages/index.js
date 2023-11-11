056237403ff202e9bac14c133d660196f6d9961f
Sashank Agarwal <sashankagarwal97@gmail.com>
2023-11-11 10:13:09 -0800

    enable analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+import { Analytics } from '@vercel/analytics/react';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -16,6 +17,7 @@ const IndexPage = ({ location }) => (
       <Featured />
       <Projects />
       <Contact />
+      <Analytics />
     </StyledMainContainer>
   </Layout>
 );
